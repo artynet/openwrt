c2d53bd0f97e2fbd3f60c342a75fb5bea8a02186
Arturo Rinaldi <arty.net2@gmail.com>
2017-06-20 14:37:38 +0200

    toplevel.mk : adding deflocal function to use lininoconfig

--- include/toplevel.mk
+++ include/toplevel.mk
@@ -109,6 +109,11 @@ deflinino: scripts/config/conf prepare-tmpinfo FORCE
 	@if [ -e $(HOME)/.openwrt/deflinino ]; then cp $(HOME)/.openwrt/deflinino .config; fi
 	$< --defconfig=.config Config.in
 
+deflocal: scripts/config/conf prepare-tmpinfo FORCE
+	touch .config
+	@if [ -e $(TOPDIR)/configfiles/lininoconfig ]; then cp $(TOPDIR)/configfiles/lininoconfig .config; fi
+	$< --defconfig=.config Config.in
+
 confdefault-y=allyes
 confdefault-m=allmod
 confdefault-n=allno
